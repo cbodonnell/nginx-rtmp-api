cfe897bce3d7814a919cac6ec3a2be1df3d26f51
cbodonnell <cheebzmusic@gmail.com>
2021-01-19 22:49:38 -0500

    /stream/{user_id} and /streams/{user_id}

--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.15
 require (
 	github.com/gorilla/mux v1.8.0
 	github.com/jackc/pgx/v4 v4.10.1
+	github.com/lib/pq v1.9.0
 	github.com/otiai10/copy v1.4.2
 )
--- models.go
+++ models.go
@@ -1,11 +1,16 @@
 package main
 
+import (
+	"database/sql"
+	"time"
+)
+
 // Configuration struct
 type Configuration struct {
-	Debug          bool          `json:"debug"`
-	Port	       int           `json:"port"`
-	Db             DataSource    `json:"db"`
-	Filesystem     bool          `json:"filesystem"`
+	Debug      bool       `json:"debug"`
+	Port       int        `json:"port"`
+	Db         DataSource `json:"db"`
+	Filesystem bool       `json:"filesystem"`
 }
 
 // DataSource struct
@@ -15,4 +20,14 @@ type DataSource struct {
 	User     string `json:"user"`
 	Password string `json:"password"`
 	Dbname   string `json:"dbname"`
+}
+
+// Stream struct
+type Stream struct {
+	ID        int          `json:"id"`
+	Title     string       `json:"title"`
+	UserID    int          `json:"user_id"`
+	Live      bool         `json:"live"`
+	StartTime time.Time    `json:"start_time"`
+	EndTime   sql.NullTime `json:"end_time"`
 }
